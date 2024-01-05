abbbd09cdb56edc03cd4c3266eeb83f0fefa3ee4
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-01-05 14:13:16 -0300

    adição das funções pegar todos os membros, pegar um membro e deletar um membro no arquivo de api.

--- api/api.ts
+++ api/api.ts
@@ -1,21 +1,42 @@
-import { FormSchemaSignIn } from "@/lib/schemas";
+import { FormSchemaRegister, FormSchemaSignIn } from "@/lib/schemas";
 import { Member } from "@/types/Member";
 import axios from "axios";
 
-export const http = axios.create({
-    baseURL: process.env.BASE_API,
+export const req = axios.create({
+    baseURL: process.env.BASE_API_NODE,
 });
 
 export const api = {
     login: async (data: FormSchemaSignIn) => {
-        const results = await http.post("/login", data);
+        const results = await req.post("/login", data);
         return results.data;
     },
 
     getAllMembers: async (): Promise<Member[] | undefined> => {
         try {
-            const results = await http.get("/members");
+            const results = await req.get("/members");
             return results.data as Member[];
+        } catch (error) {}
+    },
+
+    createMember: async (data: FormSchemaRegister) => {
+        try {
+            const results = await req.post("/members", data);
+            return results.data as Member;
+        } catch (error) {}
+    },
+
+    getOneMember: async (id: string) => {
+        try {
+            const results = await req.get(`/members/${id}`);
+            return results.data as Member;
+        } catch (error) {}
+    },
+
+    deleteMember: async (id: number) => {
+        try {
+            const results = await req.delete(`/members/${id}`);
+            return results;
         } catch (error) {}
     },
 };
