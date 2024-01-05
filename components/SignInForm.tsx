6e65fae0ba999989a1ee6f337127ce885254827e
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-01-05 14:12:14 -0300

    criação de um botão no formulário de login que altera o tipo do campo de password.

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -14,12 +14,15 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { type FormSchemaSignIn, formSchemaSignIn } from "@/lib/schemas";
+import { useState } from "react";
+import { LucideEye, LucideEyeOff } from "lucide-react";
 
 type Props = {
     onSubmit: (values: FormSchemaSignIn) => void;
 };
 
 const SignInForm = ({ onSubmit }: Props) => {
+    const [type, setType] = useState<"password" | "text">("password");
     const form = useForm<FormSchemaSignIn>({
         resolver: zodResolver(formSchemaSignIn),
         defaultValues: {
@@ -27,6 +30,11 @@ const SignInForm = ({ onSubmit }: Props) => {
             password: "",
         },
     });
+
+    const handleToggleTypePassword = () => {
+        setType((prev) => (prev === "password" ? "text" : "password"));
+    };
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
@@ -53,11 +61,26 @@ const SignInForm = ({ onSubmit }: Props) => {
                         <FormItem>
                             <FormLabel>Senha:</FormLabel>
                             <FormControl>
-                                <Input
-                                    type="text"
-                                    {...field}
-                                    placeholder="Digite sua senha"
-                                />
+                                <div className="relative">
+                                    <Input
+                                        type={type}
+                                        {...field}
+                                        placeholder="Digite sua senha"
+                                    />
+                                    <Button
+                                        type="button"
+                                        variant="ghost"
+                                        size="icon"
+                                        className="p-1 absolute top-1.5 right-2 w-auto h-auto"
+                                        onClick={handleToggleTypePassword}>
+                                        {field.value &&
+                                            (type === "password" ? (
+                                                <LucideEye size={20} />
+                                            ) : (
+                                                <LucideEyeOff size={20} />
+                                            ))}
+                                    </Button>
+                                </div>
                             </FormControl>
                             <FormMessage />
                         </FormItem>
