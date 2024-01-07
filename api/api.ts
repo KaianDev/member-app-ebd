412eaa0a5df5cadafd1102cdb489638780f64c5e
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-01-07 17:01:08 -0300

    criação das funções que farão as requisições para os endpoints de atualizar e pesquisar membros

--- api/api.ts
+++ api/api.ts
@@ -1,42 +1,61 @@
-import { FormSchemaRegister, FormSchemaSignIn } from "@/lib/schemas";
+import { FormMemberSchema, FormSchemaSignIn } from "@/lib/schemas";
 import { Member } from "@/types/Member";
+import { SearchMember } from "@/types/SearchMember";
 import axios from "axios";
 
 export const req = axios.create({
-    baseURL: process.env.BASE_API_NODE,
+  baseURL: process.env.BASE_API_NODE || "http://localhost:4000",
 });
 
 export const api = {
-    login: async (data: FormSchemaSignIn) => {
-        const results = await req.post("/login", data);
-        return results.data;
-    },
+  login: async (data: FormSchemaSignIn) => {
+    const results = await req.post("/login", data);
+    return results.data;
+  },
 
-    getAllMembers: async (): Promise<Member[] | undefined> => {
-        try {
-            const results = await req.get("/members");
-            return results.data as Member[];
-        } catch (error) {}
-    },
+  getAllMembers: async (): Promise<Member[] | undefined> => {
+    try {
+      const results = await req.get("/members");
+      return results.data as Member[];
+    } catch (error) {}
+  },
 
-    createMember: async (data: FormSchemaRegister) => {
-        try {
-            const results = await req.post("/members", data);
-            return results.data as Member;
-        } catch (error) {}
-    },
+  createMember: async (data: FormMemberSchema) => {
+    try {
+      const results = await req.post("/members", data);
+      return results.data as Member;
+    } catch (error) {}
+  },
 
-    getOneMember: async (id: string) => {
-        try {
-            const results = await req.get(`/members/${id}`);
-            return results.data as Member;
-        } catch (error) {}
-    },
+  getOneMember: async (id: string) => {
+    const results = await req.get(`/members/${id}`);
+    const member = results.data as Member;
+    if (member.id) {
+      return member;
+    }
+    throw new Error("Ocorreu um erro");
+  },
 
-    deleteMember: async (id: number) => {
-        try {
-            const results = await req.delete(`/members/${id}`);
-            return results;
-        } catch (error) {}
-    },
+  deleteMember: async (id: number) => {
+    try {
+      const results = await req.delete(`/members/${id}`);
+      return results;
+    } catch (error) {}
+  },
+
+  updateMember: async (id: number, data: FormMemberSchema) => {
+    try {
+      const results = await req.put(`/members/${id}`, data);
+      return results.data as Member;
+    } catch (error) {}
+  },
+
+  searchMember: async (searchParams: SearchMember) => {
+    try {
+      const results = await req.get(`/members/search`, {
+        params: searchParams,
+      });
+      return results.data as Member[];
+    } catch (error) {}
+  },
 };
