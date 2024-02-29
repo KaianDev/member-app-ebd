129fb64170a5dc224f7957a6abab4fad48cce166
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-02-29 17:33:17 -0300

    fix: rename token name

--- helpers/getAccessToken.ts
+++ helpers/getAccessToken.ts
@@ -2,7 +2,9 @@ import { NextRequest } from "next/server";
 import { decode } from "next-auth/jwt";
 
 export const getAccessToken = async (req: NextRequest) => {
-  const token = req.cookies.get("next-auth.session-token")?.value;
+  const token = req.cookies.get(
+    `${process.env.NEXT_PUBLIC_TOKEN_NAME}.session-token`
+  )?.value;
 
   if (!token) return undefined;
 
